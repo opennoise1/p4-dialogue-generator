792cb35a7df8d55effaa8277cc55d8e56d02b5fa
opennoise1 <ckopcow@gmail.com>
2022-03-22 14:17:57 -0400

    Continued to add more portrait widths and positions

--- utils/portraitPositions.js
+++ utils/portraitPositions.js
@@ -1,8 +1,6 @@
 const findPosition = (version, char, emote, costume) => {
   switch (char) {
-    case 'Adachi': {
-      return version === 'golden' ? [795, 138] : [870, 117];
-    }
+    case 'Adachi': return version === 'golden' ? [795, 138] : [870, 117];
     case 'Ai': {
       if (costume === 'Summer Uniform') {
         return version === 'golden' ? [793, 143] : [870, 122];
@@ -11,19 +9,53 @@ const findPosition = (version, char, emote, costume) => {
     }
     case 'Ayane': {
       if (costume === 'Spring Uniform') {
-        return version === 'golden' ? [815, 157] : [890, 137];
+        return version === 'golden' ? [815, 159] : [890, 139];
       }
       if (costume === 'Summer Uniform') {
         return version === 'golden' ? [815, 159] : [890, 139];
       }
-      return version === 'golden' ? [815, 158] : [890, 137];
+      return version === 'golden' ? [815, 160] : [890, 139];
     }
     case 'Chie': {
-      return version === 'golden' ? [795, 174] : [795, 135];
+      if (emote === 'Miscellaneous') return version === 'golden' ? [802, 173] : [822, 151]; 
+      switch (costume) {
+        case 'Epilogue': return version === 'golden' ? [793, 171] : [813, 150];
+        case 'New Years': return version === 'golden' ? [821, 171] : [841, 150];
+        case 'Spring Casual': return version === 'golden' ? [802, 172] : [822, 150];
+        case 'Spring Uniform': return version === 'golden' ? [802, 172] : [822, 150];
+        case 'Spring Uniform (Glasses)': return version === 'golden' ? [802, 172] : [822, 150];
+        case 'Ski Trip': return version === 'golden' ? [774, 96] : [794, 75];
+        case 'Summer Casual': return version === 'golden' ? [807, 172] : [827, 150];
+        case 'Summer Uniform': return version === 'golden' ? [807, 172] : [827, 150];
+        case 'Summer Uniform (Glasses)': return version === 'golden' ? [807, 172] : [827, 150];
+        case 'Swimsuit': return version === 'golden' ? [807, 172] : [827, 150];
+        case 'Winter Casual': return version === 'golden' ? [787, 172] : [807, 150];
+        case 'Winter Uniform': return version === 'golden' ? [795, 171] : [815, 151]; 
+        case 'Yukata': return version === 'golden' ? [803, 172] : [823, 153];
+        default: return version === 'golden' ? [802, 173] : [822, 151];
+      }
+    }
+    case 'Chihiro': return version === 'golden' ? [745, 171] : [800, 150];
+    case 'Daisuke': {
+      if (costume === 'Soccer Practice') {
+        return version === 'golden' ? [780, 167] : [849, 147];
+      }
+      return version === 'golden' ? [775, 167] : [844, 147];
+    }
+    case 'Mr. Edogawa': return version === 'golden' ? [735, 182] : [850, 161];
+    case 'Eri': return version === 'golden' ? [785, 150] : [850, 130];
+    case 'Fox': return version === 'golden' ? [770, 163] : [865, 142];
+    case 'Hanako': return version === 'golden' ? [740, 225] : [825, 205];
+    case 'Hisano': {
+      if (emote === 'Sad') {
+        return version === 'golden' ? [801, 186] : [881, 166];
+      } 
+      return version === 'golden' ? [800, 185] : [880, 165];
     }
     case 'Kanji': {
-      if (costume === 'New Years') { return version === 'golden' ? [795, 145] : [860, 125] };
-
+      if (costume === 'New Years') { 
+        return version === 'golden' ? [795, 145] : [860, 125] 
+      }
       return version === 'golden' ? [795, 135] : [870, 115];
     }
     case 'Naoto': {
--- utils/portraitWidths.js
+++ utils/portraitWidths.js
@@ -2,12 +2,39 @@ const findWidth = (char, emote, costume) => {
   switch (char) {
     case 'Ai': {
       if (costume !== 'Summer Uniform') return 312;
+      return 300;
     }
     case 'Ayane': {
-      if (costume === 'Spring Uniform') return 225;
+      if (costume === 'Spring Uniform') return 223;
       if (costume === 'Summer Uniform') return 230;
-      return 235;
+      return 233;
     }
+    case 'Chie': {
+      switch (costume) {
+        case 'Epilogue': return 275;
+        case 'New Years': return 255;
+        case 'Ski Trip': return 336;
+        case 'Spring Casual': return 290;
+        case 'Spring Uniform': return 290;
+        case 'Spring Uniform (Glasses)': return 290;
+        case 'Summer Casual': return 280;
+        case 'Summer Uniform': return 280;
+        case 'Summer Uniform (Glasses)': return 280;
+        case 'Swimsuit': return 280;
+        case 'Winter Casual': return 312;
+        case 'Winter Uniform': return 300;
+        case 'Yukata': return 278;
+        default: return 290;
+      }
+    }
+    case 'Mr. Edogawa': return 335;
+    case 'Eri': return 260;
+    case 'Fox': return 320;
+    case 'Hanako': {
+      if (costume === 'Summer Uniform') return 339;
+      return 330;
+    }
+    case 'Hisano': return 320;
     default: return 300;
   }
 }
